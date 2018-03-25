608853db7e6ca90dc3188aa4d538d8b6402f644d
SimSonic <simsonic@yandex.ru>
2018-03-25 17:08:49 +0700

    Caching, building server directory structure.

--- src/main/java/ru/simsonic/minecraft/yivemirror/RunMojo.java
+++ src/main/java/ru/simsonic/minecraft/yivemirror/RunMojo.java
@@ -16,12 +16,26 @@ import java.io.IOException;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 @Mojo(name = "run")
 public class RunMojo extends AbstractMojo {
 
     public static final String DEFAULT_SERVER_TYPE = "spigot";
     public static final String DEFAULT_SERVER_VERSION = "latest";
+    public static final String DEFAULT_SUBDIRECTORY = "server";
+    public static final String DEFAULT_RESOURCES = "server-resources";
+
+    @Parameter(defaultValue = "${project.basedir}", readonly = true, required = true)
+    public File projectDirectory;
+
+    @Parameter(defaultValue = "${project.build.directory}", readonly = true, required = true)
+    public File buildDirectory;
+
+    @Parameter(defaultValue = "${project.build.finalName}", readonly = true, required = true)
+    public String finalName;
 
     /**
      * Spigot, PaperSpigot, Thermos, etc.
@@ -35,26 +49,15 @@ public class RunMojo extends AbstractMojo {
     @Parameter(property = "serverVersion", defaultValue = DEFAULT_SERVER_VERSION)
     public String serverVersion;
 
-    @Override
-    public void execute() throws MojoExecutionException {
-        try {
-            download();
-            install();
-            run();
-        } catch (Exception ex) {
-            throw new MojoExecutionException("Internal plugin error.", ex);
-        }
-    }
+    @Parameter(property = "directory", defaultValue = DEFAULT_SUBDIRECTORY)
+    public String directory;
 
-    private void download() throws IOException {
-        info("Downloading '%s' version '%s' ...", serverType, serverVersion);
-        String filename = String.format("%s-%s.jar", serverType, serverVersion);
-        String url = String.format("https://yivesmirror.com/files/%s/%s", serverType, filename);
-        info("Downloading url = %s", url);
-        File target = new File("target", filename);
-        info("Target file is %s", target.getAbsolutePath());
+    @Parameter(property = "resources", defaultValue = DEFAULT_RESOURCES)
+    public String resources;
+
+    public static void downloadFile(String sourceUrl, File target) throws IOException {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpUriRequest request = new HttpGet(url);
+            HttpUriRequest request = new HttpGet(sourceUrl);
             CloseableHttpResponse response = httpClient.execute(request);
             ReadableByteChannel sourceChannel = Channels.newChannel(response.getEntity().getContent());
             FileChannel destinationChannel = new FileOutputStream(target).getChannel();
@@ -62,11 +65,75 @@ public class RunMojo extends AbstractMojo {
         }
     }
 
-    private void install() {
-        getLog().info("Installing ...");
+    @Override
+    public void execute() throws MojoExecutionException {
+        try {
+            String filename = String.format("%s-%s.jar", serverType, serverVersion);
+            File cached = searchInCache(filename);
+            if (!cached.isFile()) {
+                download(filename, cached);
+            }
+
+            File serverDir = new File(buildDirectory, directory);
+            install(cached, serverDir);
+
+            run(cached, serverDir);
+        } catch (Exception ex) {
+            throw new MojoExecutionException("Internal plugin error.", ex);
+        }
     }
 
-    private void run() {
+    private File searchInCache(String filename) {
+        String mavenPluginFile = RunMojo.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+        File cacheDirectory = new File(new File(mavenPluginFile).getParentFile(), "cache");
+        cacheDirectory.mkdirs();
+        File result = new File(cacheDirectory, filename);
+        debug("Location of cached version: %s", result);
+        return result;
+    }
+
+    private void download(String filename, File target) throws IOException {
+        String url = String.format("https://yivesmirror.com/files/%s/%s", serverType, filename);
+        info("Downloading from %s into %s", url, target);
+        target.getParentFile().mkdirs();
+        downloadFile(url, target);
+    }
+
+    private void install(File serverJar, File serverDir) throws IOException, MojoExecutionException {
+        getLog().info("Installing ...");
+
+        serverDir.mkdirs();
+
+        File resourcesDir = new File(projectDirectory, resources);
+        if (resourcesDir.isDirectory()) {
+            copyFolder(resourcesDir, serverDir);
+        }
+
+        File compiledPlugin = new File(buildDirectory, finalName + ".jar");
+        if (!compiledPlugin.isFile()) {
+            throw new MojoExecutionException("Compiled plugin .jar is absent.");
+        }
+        File pluginsDir = new File(serverDir, "plugins");
+        pluginsDir.mkdirs();
+        File installedPlugin = new File(pluginsDir, finalName + ".jar");
+        Files.copy(compiledPlugin.toPath(), installedPlugin.toPath());
+    }
+
+    private void copyFolder(File sourceDir, File targetDir) throws IOException {
+        try (Stream<Path> stream = Files.walk(sourceDir.toPath())) {
+            stream.forEach(sourcePath -> copyFile(sourcePath, targetDir.toPath()));
+        }
+    }
+
+    private void copyFile(Path sourcePath, Path destPath) {
+        try {
+            Files.copy(sourcePath, destPath.resolve(sourcePath.relativize(destPath)));
+        } catch (Exception ex) {
+            error(ex.getMessage());
+        }
+    }
+
+    private void run(File serverJar, File serverDir) {
         getLog().info("Running ...");
     }
 
